c7ac60b923adbba7ec828ae3f95ee9901d61b5e2
Rodrigo Goncalves <rodrigo.rebes@gmail.com>
2021-08-12 13:15:31 -0300

    Add desafio

---
+++ RepositoriosTakeAPI/Controllers/RepositoriosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RepositoriosTakeAPI.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RepositoriosTakeAPI.Controllers
+{
+    [Route("api/repositoriostake")]
+    [ApiController]
+    public class RepositoriosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<RepositorioVM>> Get()
+        {
+            using (var client = new HttpClient())
+            {
+                var repositorios = new List<RepositorioVM>();
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                var baseUrl = "https://api.github.com/users/takenet/repos";
+                var response = client.GetStringAsync(baseUrl).Result;
+                var responseList = JsonConvert.DeserializeObject<List<RepositorioVM>>(response);
+
+                repositorios = responseList.Where(x => !string.IsNullOrEmpty(x.Linguagem) && x.Linguagem.Equals("C#", System.StringComparison.OrdinalIgnoreCase)).OrderBy(x => x.DataCriacao).ToList();
+
+                return repositorios;
+            }
+        }
+
+    }
+}
---
+++ RepositoriosTakeAPI/ViewModels/RepositorioVM.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoriosTakeAPI.ViewModels
+{
+    public class RepositorioVM
+    {
+        [JsonProperty("full_name")]
+        public string NomeRepositorio { get; set; }
+
+        [JsonProperty("description")]
+        public string Descricao { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime DataCriacao { get; set; }
+
+        [JsonProperty("language")]
+        public string Linguagem { get; set; }
+
+        [JsonProperty("owner")]
+        public ImagemVM Imagem { get; set; }
+    }
+
+    public class ImagemVM
+    {
+        [JsonProperty("avatar_url")]
+        public string Avatar { get; set; }
+    }
+}
